c37f5231432ca5a49ff0a0e07c5d204552d8427f
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2026-02-16 09:46:05 +0100

    chore: run after_install method after install

--- erpnext_kassenbuch/hooks.py
+++ erpnext_kassenbuch/hooks.py
@@ -83,7 +83,7 @@ app_license = "mit"
 # ------------
 
 # before_install = "erpnext_kassenbuch.install.before_install"
-# after_install = "erpnext_kassenbuch.install.after_install"
+after_install = "erpnext_kassenbuch.install.after_install"
 
 # Uninstallation
 # ------------
