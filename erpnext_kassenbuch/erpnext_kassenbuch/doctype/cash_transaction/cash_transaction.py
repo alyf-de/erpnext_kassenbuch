84e98a667f44ccf636a1515760db10abc39ff879
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2026-02-16 09:52:47 +0100

    fix: import get_default_cost_center

--- erpnext_kassenbuch/erpnext_kassenbuch/doctype/cash_transaction/cash_transaction.py
+++ erpnext_kassenbuch/erpnext_kassenbuch/doctype/cash_transaction/cash_transaction.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from erpnext.accounts.utils import get_default_cost_center
+from erpnext import get_default_cost_center
 from frappe.model.document import Document
 from frappe.utils import flt
 
